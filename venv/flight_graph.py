07f2fb2d1c7c857c51fc67bbb2aee8e2db45df2b
Arush <arushadabalar@gmail.com>
2023-09-23 21:55:12 -0500

    four commit

---
+++ venv/flight_graph.py
@@ -0,0 +1,23 @@
+import airport_scraper
+from Flight import Flight
+from Airport import Airport
+
+class flight_graph:
+    def __init__(self):
+        f = open("airports.txt")
+        self.flightGraph = {}
+        for code in f.readline():
+            this.flightGraph[code] = airport_scraper.get_airport(code)
+        f.close()
+
+    def get_flight(self,start_airport,end_airport,carbon_weight,time_weight,cost_weight):
+        unvisited = set({})
+        distances = {}
+        for code in self.flightGraph:
+            distances[code] = math.inf
+            unvisited.add(code)
+        while len(unvisited) > 0:
+            currAirport
+
+
+
